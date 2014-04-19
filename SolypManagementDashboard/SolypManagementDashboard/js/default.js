c850cc5686c70cb323d98e5ef2aa152c8e78c050
kiranbonu <kiran.bonu@gmail.com>
2014-04-19 18:54:57 +0200

    Exit Button and default background colour,text

--- SolypManagementDashboard/SolypManagementDashboard/js/default.js
+++ SolypManagementDashboard/SolypManagementDashboard/js/default.js
@@ -5,6 +5,7 @@
 
     var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
+  
 
     app.onactivated = function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
@@ -29,4 +30,6 @@
     };
 
     app.start();
+   
+    
 })();
