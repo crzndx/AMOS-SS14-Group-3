bf9528a91bc759f254f6c4fc30bda2063c36c8bd
crzndx <crzndx@gmail.com>
2014-04-29 01:04:46 +0200

    navigation bugfix

--- SolypManagementDashboard/SolypManagementDashboard/js/homescreenData.js
+++ SolypManagementDashboard/SolypManagementDashboard/js/homescreenData.js
@@ -79,7 +79,7 @@
             { group: sampleGroups[0], title: "Competitor Cards", subtitle: "Shows competitors in form of trading-cards to compare properties easily", description: itemDescription, content: itemContent, backgroundImage: lightGray, path: "/pages/competitorCards/competitorCards.html" },
             { group: sampleGroups[0], title: "Empty Page", subtitle: "Item unterschrift ohne Bedeutung", description: itemDescription, content: itemContent, backgroundImage: darkGray, path: "/pages/testSeite/page.html" },
 
-            { group: sampleGroups[1], title: "Item Title: 1", subtitle: "Item Subtitle: 1", description: itemDescription, content: itemContent, backgroundImage: darkGray },
+            { group: sampleGroups[1], title: "Dummy Page", subtitle: "Have a look at the dummy page", description: itemDescription, content: itemContent, backgroundImage: darkGray, path: "/pages/newPageDummy/newDummyHTML.html" },
             { group: sampleGroups[1], title: "Item Title: 2", subtitle: "Item Subtitle: 2", description: itemDescription, content: itemContent, backgroundImage: mediumGray },
             { group: sampleGroups[1], title: "Item Title: 3", subtitle: "Item Subtitle: 3", description: itemDescription, content: itemContent, backgroundImage: lightGray },
 
--- SolypManagementDashboard/SolypManagementDashboard/pages/newPageDummy/newDummyJS.js
+++ SolypManagementDashboard/SolypManagementDashboard/pages/newPageDummy/newDummyJS.js
@@ -1,20 +1,24 @@
 ﻿(function () {
     "use strict";
 
-    WinJS.UI.Pages.define("/pages/newPageDummy/newDummyHTML.html", {  // Edit the link name
+    WinJS.UI.Pages.define("/pages/itemDetail/itemDetail.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
             var item = HomescreenData.resolveItemReference(options.item);
 
+            // Parses the title of the subpage from the data file
+            element.querySelector(".titlearea .pagetitle").textContent = item.title;
+
             // Add useful content here
-            // what should be done after pageload? e.g. start d3.js or other services
+            // what should be done after pageload? does more content need to be rendered?
+            // start other js services?
 
-
-            // deprecated. 
-            //element.querySelector(".titlearea .pagetitle").textContent = item.title;
-
-            // TODO: Initialize the page here.
         }
     });
+
+
+    // some more librarys/js starts here
+
+
 })();
