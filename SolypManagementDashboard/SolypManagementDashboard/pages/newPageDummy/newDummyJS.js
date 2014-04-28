8ee863860e7ad96e5b288bf8e6a8b5380ef5025d
crzndx <crzndx@gmail.com>
2014-04-29 00:33:55 +0200

    changed project structure to have one subfolder for each subpage. please follow this structure when creating a new subpage. the folder newPageDummy is a template for new subpages you may create. copy the folder, paste and rename it to create a new subpage thx

---
+++ SolypManagementDashboard/SolypManagementDashboard/pages/newPageDummy/newDummyJS.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/newPageDummy/newDummyHTML.html", {  // Edit the link name
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            var item = HomescreenData.resolveItemReference(options.item);
+
+            // Add useful content here
+            // what should be done after pageload? e.g. start d3.js or other services
+
+
+            // deprecated. 
+            //element.querySelector(".titlearea .pagetitle").textContent = item.title;
+
+            // TODO: Initialize the page here.
+        }
+    });
+})();
