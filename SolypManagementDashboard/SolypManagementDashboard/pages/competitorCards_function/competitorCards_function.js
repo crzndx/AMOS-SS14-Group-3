de9df0c548c6b46dba775396d61b69c38505c903
nurita <nur.ghossien@gmail.com>
2014-05-15 18:25:50 +0200

    release_05

--- SolypManagementDashboard/SolypManagementDashboard/pages/competitorCards_function/competitorCards_function.js
+++ SolypManagementDashboard/SolypManagementDashboard/pages/competitorCards_function/competitorCards_function.js
@@ -212,35 +212,7 @@
                           .attr("class", "signup")
                           .text("More Info");
 
-                 //drawing the barchart
-
-                     //size of the bar chart 70*160
-
-                     var w = 160;
-                     var h = 70;
-                     var barpadding = 10;
-                     var four = [0,0,0,0]
-
-                     //create the svg
-                     var svg = d3.select(".barchart")
-                                 .append("svg")
-                                 .attr("width", w)
-                                 .attr("height", h);
-                     // we create a rectangle for each entry of the data
-                     svg.selectAll("rect")
-                         .data(d.Period1.quarterRevenue)
-                         .enter()
-                         .append("rect")
-                         //position on the x axis
-                         .attr("x", function (d, i) {
-                             return i * (w / 4); //like this it adjust to the amount of data we have
-                         })
-                         .attr("y", function (d) {
-                             return h - d;
-                         })
-                         .attr("width", w / 4 - barpadding)
-                         .attr("height", function (d) { return d.Period1.quarterRevenue.Q1; })
-                         .attr("fill", "teal");
+                
 
 
                  
@@ -248,6 +220,36 @@
         } //here the card display ends
     
    
+    function createbarchart(data) {
+        //drawing the barchart
 
+        //size of the bar chart 70*160
+
+        var w = 160;
+        var h = 70;
+        var barpadding = 10;
+        var four = [0, 0, 0, 0]
+
+        //create the svg
+        var svg = d3.select(".barchart")
+                    .append("svg")
+                    .attr("width", w)
+                    .attr("height", h);
+        // we create a rectangle for each entry of the data
+        svg.selectAll("rect")
+            .data(d.Period1.quarterRevenue)
+            .enter()
+            .append("rect")
+            //position on the x axis
+            .attr("x", function (d, i) {
+                return i * (w / 4); //like this it adjust to the amount of data we have
+            })
+            .attr("y", function (d) {
+                return h - d;
+            })
+            .attr("width", w / 4 - barpadding)
+            .attr("height", function (d) { return d.Period1.quarterRevenue.Q1; })
+            .attr("fill", "teal");
+    }
 
 })();
