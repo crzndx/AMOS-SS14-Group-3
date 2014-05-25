9c11bcdd279fcee75fc2243c3fbb3e64ac9c5840
nurita <nur.ghossien@gmail.com>
2014-05-25 18:12:42 +0200

    creating AllCards view using listviw winjs

--- SolypManagementDashboard/SolypManagementDashboard/pages/allCards/allCards.js
+++ SolypManagementDashboard/SolypManagementDashboard/pages/allCards/allCards.js
@@ -20,46 +20,18 @@
             /*End of the slideBar*/
 
             //listview
-            var myData = new WinJS.Binding.List([
+            var itemArray = [
+        { title: "Marvelous Mint", text: "Gelato", picture: "/images/fruits/60Mint.png" },
+        { title: "Succulent Strawberry", text: "Sorbet", picture: "/images/fruits/60Strawberry.png" },
         { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/fruits/60Banana.png" },
         { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" },
-        { title: "Marvelous Mint", text: "Gelato", picture: "/images/fruits/60Mint.png" },
         { title: "Creamy Orange", text: "Sorbet", picture: "/images/fruits/60Orange.png" },
-        { title: "Succulent Strawberry", text: "Sorbet", picture: "/images/fruits/60Strawberry.png" },
         { title: "Very Vanilla", text: "Ice Cream", picture: "/images/fruits/60Vanilla.png" },
         { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/fruits/60Banana.png" },
-        { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" },
-        { title: "Marvelous Mint", text: "Gelato", picture: "/images/fruits/60Mint.png" },
-        { title: "Creamy Orange", text: "Sorbet", picture: "/images/fruits/60Orange.png" },
-        { title: "Succulent Strawberry", text: "Sorbet", picture: "/images/fruits/60Strawberry.png" },
-        { title: "Very Vanilla", text: "Ice Cream", picture: "/images/fruits/60Vanilla.png" },
-        { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/fruits/60Banana.png" },
-        { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" },
-        { title: "Marvelous Mint", text: "Gelato", picture: "/images/fruits/60Mint.png" },
-        { title: "Creamy Orange", text: "Sorbet", picture: "/images/fruits/60Orange.png" },
-        { title: "Succulent Strawberry", text: "Sorbet", picture: "/images/fruits/60Strawberry.png" },
-        { title: "Very Vanilla", text: "Ice Cream", picture: "/images/fruits/60Vanilla.png" },
-        { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/fruits/60Banana.png" },
-        { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" },
-        { title: "Marvelous Mint", text: "Gelato", picture: "/images/fruits/60Mint.png" },
-        { title: "Creamy Orange", text: "Sorbet", picture: "/images/fruits/60Orange.png" },
-        { title: "Succulent Strawberry", text: "Sorbet", picture: "/images/fruits/60Strawberry.png" },
-        { title: "Very Vanilla", text: "Ice Cream", picture: "/images/fruits/60Vanilla.png" },
-        { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/fruits/60Banana.png" },
-        { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" },
-        { title: "Marvelous Mint", text: "Gelato", picture: "/images/fruits/60Mint.png" },
-        { title: "Creamy Orange", text: "Sorbet", picture: "/images/fruits/60Orange.png" },
-        { title: "Succulent Strawberry", text: "Sorbet", picture: "/images/fruits/60Strawberry.png" },
-        { title: "Very Vanilla", text: "Ice Cream", picture: "/images/fruits/60Vanilla.png" },
-        { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/fruits/60Banana.png" },
-        { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" },
-        { title: "Marvelous Mint", text: "Gelato", picture: "/images/fruits/60Mint.png" },
-        { title: "Creamy Orange", text: "Sorbet", picture: "/images/fruits/60Orange.png" },
-        { title: "Succulent Strawberry", text: "Sorbet", picture: "/images/fruits/60Strawberry.png" },
-        { title: "Very Vanilla", text: "Ice Cream", picture: "/images/fruits/60Vanilla.png" }
-            ]);
+        { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" }
+            ];
 
-            showAllCards(myData);
+            showAllCards(itemArray);
 
         }
     });
@@ -68,18 +40,18 @@
     // some more librarys/js starts here
     function showAllCards(data) {
 
-        var grouped = data.createGrouped(function (item) {
-            return item.title.toUpperCase().charAt(0);
-        }, function (item) {
-            return {
-                title: item.title.toUpperCase().charAt(0)
-            };
-        }, function (left, right) {
-            return left.charCodeAt(0) - right.charCodeAt(0);
-        });
+        var items = [];
+
+        // Generate 160 items
+        for (var i = 0; i < 20; i++) {
+            itemArray.forEach(function (item) {
+                items.push(item);
+            });
+        }
+
 
         WinJS.Namespace.define("Sample.ListView", {
-            data: grouped
+            data: new WinJS.Binding.List(items)
         });
         WinJS.UI.processAll();
     }
