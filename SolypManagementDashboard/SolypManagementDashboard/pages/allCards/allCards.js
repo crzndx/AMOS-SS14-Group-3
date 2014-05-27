cc65aff2170fa05bbcd036022d4899139461d76d
nurita <nur.ghossien@gmail.com>
2014-05-27 15:03:11 +0200

    first working allcards visualization in a new tile with sample data

--- SolypManagementDashboard/SolypManagementDashboard/pages/allCards/allCards.js
+++ SolypManagementDashboard/SolypManagementDashboard/pages/allCards/allCards.js
@@ -1,60 +1,44 @@
 ﻿(function () {
     "use strict";
 
-    WinJS.UI.Pages.define("/pages/itemDetail/itemDetail.html", {
+    // Code from TryWinJS
+    var itemArray = [
+       { title: "Marvelous Mint", text: "Gelato", picture: "/images/fruits/60Mint.png" },
+       { title: "Succulent Strawberry", text: "Sorbet", picture: "/images/fruits/60Strawberry.png" },
+       { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/fruits/60Banana.png" },
+       { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" },
+       { title: "Creamy Orange", text: "Sorbet", picture: "/images/fruits/60Orange.png" },
+       { title: "Very Vanilla", text: "Ice Cream", picture: "/images/fruits/60Vanilla.png" },
+       { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/fruits/60Banana.png" },
+       { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" }
+    ];
+    var items = [];
+
+    // Generate 160 items
+    for (var i = 0; i < 20; i++) {
+        itemArray.forEach(function (item) {
+            items.push(item);
+        });
+    }
+
+    WinJS.Namespace.define("Sample.ListView", {
+        data: new WinJS.Binding.List(items)
+    });
+    WinJS.UI.processAll();
+
+
+
+    WinJS.UI.Pages.define("/pages/allCards/allCards.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
-            var item = HomescreenData.resolveItemReference(options.item);
+            //    var item = HomescreenData.resolveItemReference(options.item);
 
             // Parses the title of the subpage from the data file
-            element.querySelector(".titlearea .pagetitle").textContent = item.title;
-
-            /* Initialize App Bar */
-            slidingAppBar = document.getElementById("appBar").winControl;
-
-            slidingAppBar.getCommandById("showNextYear").addEventListener("click", function () { showNextYear(actualYear); });
-            slidingAppBar.getCommandById("showPreviousYear").addEventListener("click", function () { showPreviousYear(actualYear); });
-            slidingAppBar.getCommandById("showAllCards").addEventListener("click", function () { showAllCards(); });
-            
-            /*End of the slideBar*/
-
-            //listview
-            var itemArray = [
-        { title: "Marvelous Mint", text: "Gelato", picture: "/images/fruits/60Mint.png" },
-        { title: "Succulent Strawberry", text: "Sorbet", picture: "/images/fruits/60Strawberry.png" },
-        { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/fruits/60Banana.png" },
-        { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" },
-        { title: "Creamy Orange", text: "Sorbet", picture: "/images/fruits/60Orange.png" },
-        { title: "Very Vanilla", text: "Ice Cream", picture: "/images/fruits/60Vanilla.png" },
-        { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/fruits/60Banana.png" },
-        { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" }
-            ];
-
-            showAllCards(itemArray);
+            // element.querySelector(".titlearea .pagetitle").textContent = item.title;
 
         }
     });
-
-
-    // some more librarys/js starts here
-    function showAllCards(data) {
-
-        var items = [];
-
-        // Generate 160 items
-        for (var i = 0; i < 20; i++) {
-            itemArray.forEach(function (item) {
-                items.push(item);
-            });
-        }
-
-
-        WinJS.Namespace.define("Sample.ListView", {
-            data: new WinJS.Binding.List(items)
-        });
-        WinJS.UI.processAll();
-    }
 
 
 
