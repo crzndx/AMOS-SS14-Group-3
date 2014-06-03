c209cb8b3f1511f2c61a6b188300b679b789ffe3
nurita <nur.ghossien@gmail.com>
2014-06-03 17:09:33 +0200

    some updates on squares visualization styling, colors and size of canvas

--- SolypManagementDashboard/SolypManagementDashboard/pages/squares/squares.js
+++ SolypManagementDashboard/SolypManagementDashboard/pages/squares/squares.js
@@ -14,8 +14,16 @@
 
             //creating the squares, Im gonna try to do it without a function and later on if so create a function
 
-            var data = [105, 8, 67, 17, 55, 32];
+            var data = [105, 12, 67, 17, 55, 32];
+            var color = ["#93c747", "#b64176", "#583f95"];
+            var allColor = [];
 
+            // making the colors variable as long as elements has the data variable
+            for (var i = 0; i < data.length; i++) {
+                color.forEach(function (item) {
+                    allColor.push(item);
+                });
+            }
             //first I need to do is read the variable and sum all values so that later on I can get the percentages
 
             var sum = 0;
@@ -25,21 +33,22 @@
          
             //I have the sum of the array in sum, now its time to create the percentages 
             //lets take for granted that the width is 600px (I need to check this later) * 500px
-            var w = 600;
+            var w = 1200;
             var h = 500;
             //create somehow loop
             var handle = document.getElementById('squares');
+
             for (var i = 0; i < data.length; i++) {
                 var customSquare = document.createElement("div");
                 customSquare.style.width = w / sum * data[i]+"px";
                 customSquare.style.height = h / sum * data[i]+"px";
-                customSquare.style.background = "red";
+                customSquare.style.background = allColor[i];
                 customSquare.style.position = "relative";
                 customSquare.style.float = "left";
                 customSquare.style.border = "solid";
                 customSquare.style.borderWidth = "1px";
                 customSquare.style.borderColor = "black";
-                customSquare.innerHTML = "Hello";
+                customSquare.innerHTML = i+1;
                 handle.appendChild(customSquare);
             }
 
