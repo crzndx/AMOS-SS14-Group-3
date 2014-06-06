fc5612e37ad189608e9edb9e0cdaf501d4afc413
nurita <nur.ghossien@gmail.com>
2014-06-06 15:12:39 +0200

    Tree map done with d3, currently nor working because of DOM problems

---
+++ SolypManagementDashboard/SolypManagementDashboard/pages/squares_mike/squares_mike.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    "use strict";
+
+        
+    
+    WinJS.UI.Pages.define("/pages/squares_mike/squares_mike.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+
+            // Add useful content here
+            // what should be done after pageload? does more content need to be rendered?
+            // start other js services?
+
+           
+
+        }
+    });
+
+
+
+    // some more librarys/js starts here
+
+
+})();
