feb23872a12b1b57c5656dcbc39b797443d9253f
ow23adiz <andreas.lindsteding@fau.de>
2014-06-26 16:30:42 +0200

    Added Details-View on Tile-Comparison

--- SolypManagementDashboard/SolypManagementDashboard/pages/TileComparison/TileComparison.js
+++ SolypManagementDashboard/SolypManagementDashboard/pages/TileComparison/TileComparison.js
@@ -4,7 +4,7 @@
     var NUMBER = 0;
     var RATING = 1;
     var PERCENTAGE = 2;
-   
+
 
     function showAllCards() {
         WinJS.Navigation.navigate("pages/allCards/allCards.html");
@@ -12,31 +12,33 @@
 
 
     var customerCardsTileId = 0;
+
+    // Generates the Template for the ListView-Items
     function itemTemplateFunction(itemPromise) {
 
         return itemPromise.then(function (item) {
 
-            //the id is not well defined it only counts 3 items and not four thats why one of the cards is black
+            // customerCardsTileId determines the style-class; that's why it counts only to 4
             customerCardsTileId = (customerCardsTileId) % 4;
             customerCardsTileId++;
 
             var template = new WinJS.Binding.Template(document.getElementById("tiletemplate"));
             var tempCard = template.render(item.data, document.createElement("div"));
-            
+
             var xLabels = ["Q1", "Q1", "Q2", "Q3", "Q4"];
             // chart
-            var w = 160;
-            var h = 120;
+            var w = 300;
+            var h = 140;
             var barpadding = 10;
-            
+
             var marginTop = 30;
-            
+
             //create the svg
             var svg = d3.select(tempCard._value.querySelector(".barchart"))
                         .append("svg")
                         .attr("width", w)
                         .attr("height", h);
-            
+
             svg.append("text")
             .attr("x", (w / 2))
             .attr("y", (marginTop / 2))
@@ -44,61 +46,65 @@
             .style("font-size", "12px")
             .style("text-decoration", "underline")
             .text("Quarter Revenue");
-            
+
             //BARCHART starts here
-            
+
             // we create a rectangle for each entry of the data
-           
             svg.selectAll("rect")
                 .data(item.data.quarterRevenue)
                 .enter()
                 .append("rect")
                 //position on the x axis
-                .attr("x", function(d, i) {
+                .attr("x", function (d, i) {
                     return i * (w / 4); //like this it adjust to the amount of
 
                 })
-                .attr("y", function(d) {
+                .attr("y", function (d) {
                     return h - d;
                 })
                 .attr("width", w / 4 - barpadding)
-                .attr("height", function(d) {
+                .attr("height", function (d) {
                     return d;
                 })
                 .attr("fill", "teal");
-      
-    
+
+
             svg.selectAll("text")
                 .data(xLabels)
                 .enter()
                 .append("text")
-                .attr("x", function (d, z) { return z * (w / 4) - (w/4)/2-13; })
-                .attr("y", function(d) {
-                    return h+12;
+                .attr("x", function (d, z) { return z * (w / 4) - (w / 4) / 2 - 13; })
+                .attr("y", function (d) {
+                    return h + 12;
                 })
                 .text(function (l) { return l; });
-                
+
             svg.selectAll("p")
                 .data(item.data.quarterRevenue)
                 .enter()
                 .append("text")
-                .text(function(d) { return d; })
-                .attr("x", function(d, z) { return z * (w / 4); })
+                .text(function (d) { return d; })
+                .attr("x", function (d, z) { return z * (w / 4); })
                 .attr("y", function (d) { return h - (d); })
                 .attr("font-family", "sans-serif")
                 .attr("font-size", "11px")
-                .attr("fill","teal");
+                .attr("fill", "teal");
 
-          
+
             tempCard._value.querySelector("#plandiv").className = "plan plan" + customerCardsTileId;
-            
+
             // Create KPI
             var newInnerHTML = '<input type="hidden" class="itemid" value="' + item.data.Name + '"></input>';
             var i = 0;
+            var tops = [1, 27, 53];
             for (i = 0; i < 3; i++) {
-                newInnerHTML += generateKPIDiv(item.data.KPI[kpiBaseReference + i][0], item.data.KPI[kpiBaseReference + i][1], ((item.data.KPI[kpiBaseReference + i][2] == undefined) ? 0 : item.data.KPI[0][2]), i);
+                newInnerHTML += generateKPIDiv(
+                    item.data.KPI[kpiBaseReference + i][0],
+                    item.data.KPI[kpiBaseReference + i][1],
+                    ((item.data.KPI[kpiBaseReference + i][2] == undefined) ? 0 : item.data.KPI[0][2]),
+                    tops[i], 220, 84);
             }
-            
+
             tempCard._value.querySelector(".kpiCanvas").innerHTML = window.toStaticHTML(newInnerHTML);
             return tempCard;
         });
@@ -106,13 +112,10 @@
 
     function initTiles() {
         loadData();
-       
-
     }
 
-    
-    function loadData() {
 
+    function loadData() {
 
         var uri = new Windows.Foundation.Uri("ms-appx:///data/2011.txt");
 
@@ -128,14 +131,15 @@
 
             list2.forceLayout();
             customerCardsTileId = 0;
-            
-
 
         });
-       
+
     }
 
+    var lastInvokedTile = 0;
     function handler(eventInfo) {
+
+        lastInvokedTile = eventInfo.itemIndex;
         // Create Handles for all needed HTML-Elements
         var hndListView = document.querySelector("#customerListView").winControl;
         var hndMessageToTheUser = document.querySelector("#MessageToTheUser");
@@ -143,7 +147,6 @@
 
         // How many Items have been selected?
         var selectedItemsCount = hndListView.selection.getItems()._value.length;
-
 
         if (selectedItemsCount > 0) {
             if (selectedItemsCount == 1) {
@@ -169,7 +172,7 @@
     // Compare Tiles
     function compareTile() {
         var list2 = document.getElementById("customerListView").winControl;
-
+        var compareButton = document.getElementById("compareButton");
 
         if (selectionMode) {
             // Get the control, itemDataSource and selected items
@@ -187,13 +190,14 @@
             list2.itemDataSource = tempObj.dataSource;
 
             list2.forceLayout();
-           
+
 
         } else {
             list2.itemDataSource = rememberList;
+            compareButton.className = "MessageToTheUserInvisible";
         }
 
-        var compareButton = document.getElementById("compareButton");
+
         var temp = compareButton.innerHTML;
         compareButton.innerHTML = rememberButtonText;
 
@@ -202,29 +206,23 @@
 
         selectionMode = !selectionMode;
     }
-    
+
 
     var kpiBaseReference = 0;
     var data;
+
     function generateKPI() {
         var elems = document.getElementsByTagName('*'), i;
-        
+        var tops = [1, 27, 53];
         for (i in elems) {
             if ((' ' + elems[i].className + ' ').indexOf(' kpiCanvas ') > -1) {
 
                 var temp = elems[i].getElementsByClassName("itemid")[0];
-                var x = 0;
-                var a = temp.value;
-                var newInnerHTML = '<input type="hidden" class="itemid" value="' + a + '"></input>';
-                var b = data[x].Name;
-              
-                while (a != b) {
-                    x++;
-                    b = data[x].Name;
-                    if (x > data.count)
-                        a = b;
-                }
-                data[x].KPI[kpiBaseReference];
+
+                var newInnerHTML = '<input type="hidden" class="itemid" value="' + temp.value + '"></input>';
+
+                var x = getCustomberIdByName(temp.value);
+
 
                 var counter = 0;
 
@@ -232,8 +230,9 @@
 
                     var kpi_temp = data[x].KPI[kpiBaseReference + counter];
                     var kpi_type = (kpi_temp[2] == undefined) ? 0 : kpi_temp[2];
-                    newInnerHTML += generateKPIDiv(kpi_temp[0], kpi_temp[1], kpi_type, counter);
-                    
+
+                    newInnerHTML += generateKPIDiv(kpi_temp[0], kpi_temp[1], kpi_type, tops[counter], 225, 84);
+
                     counter++;
                 }
                 elems[i].innerHTML = window.toStaticHTML(newInnerHTML);
@@ -241,27 +240,33 @@
             }
         }
     }
-    
-    function generateKPIDiv(name, value, type, counter) {
-        var tops = [1, 27, 53];
 
+    function generateKPIDiv(name, value, type, top, nameDivWidth, valueDivWidth) {
+
+        //     debugger;
         var newInnerHTML =
-           '<div style="position: absolute; text-align: left; left:1px; top:' + tops[counter] + 'px; width: 80px; height:25px; background-color: #FFFFFF;"><b>' + name + '</b></div>';
+           '<div style="position: absolute; text-align: left; left:1px; top:' + top + 'px; width: ' + nameDivWidth
+               + 'px; height:25px; background-color: #FFFFFF;"><b>' + name + '</b></div>';
 
         switch (type) {
             case PERCENTAGE:
-                newInnerHTML += '<div style="position: absolute; text-align: left; left: 82px; top: ' + tops[counter] + 'px; width: 77px; height: 25px; background-color: #FFFFFF;">' + value + '%</div>';
+                newInnerHTML += '<div style="position: absolute; text-align: left; left: ' + (nameDivWidth + 2) + 'px; top: ' + top
+                    + 'px; width: ' + valueDivWidth + 'px; height: 25px; background-color: #FFFFFF;">' + value + '%</div>';
                 break;
             case RATING:
-                newInnerHTML += '<div style="position: absolute; text-align: left; left: 82px; top: ' + tops[counter] + 'px; width: 77px; height: 25px; background-color: #FFFFFF;">';
-                newInnerHTML += '<span class="win-small" data-win-control="WinJS.UI.Rating" data-win-options="{ userRating: ' + value + ', disabled: true}"></span></div>';
+                newInnerHTML += '<div style="position: absolute; text-align: left; left: ' + (nameDivWidth + 2) + 'px; top: ' + top
+                    + 'px; width: ' + valueDivWidth + 'px; height: 25px; background-color: #FFFFFF;">';
+                newInnerHTML += '<span class="win-small" data-win-control="WinJS.UI.Rating" data-win-options="{ userRating: ' + value
+                    + ', disabled: true}"></span></div>';
                 break;
             case NUMBER:
             default:
                 newInnerHTML +=
-                    '<div style="position: absolute; text-align: left; left: 82px; top: ' + tops[counter] + 'px; width: 77px; height: 25px; background-color: #FFFFFF;">' + value + '</div>';
+                    '<div style="position: absolute; text-align: left; left: ' + (nameDivWidth + 2) + 'px; top: ' + top + 'px; width: '
+                        + valueDivWidth + 'px; height: 25px; background-color: #FFFFFF;">' + value + '</div>';
                 break;
         }
+
         return newInnerHTML;
     }
 
@@ -276,11 +281,58 @@
         if (kpiBaseReference > 0) kpiBaseReference--;
         generateKPI();
     }
-    WinJS.Namespace.define("TemplatingExample", { itemTemplateFunction: itemTemplateFunction });
-    WinJS.Utilities.markSupportedForProcessing(TemplatingExample.itemTemplateFunction);
+    WinJS.Namespace.define("TileComparison", {
+        itemTemplateFunction: itemTemplateFunction,
+        moreInfoButton_Click: moreInfoButton_Click
+    });
+    WinJS.Utilities.markSupportedForProcessing(TileComparison.itemTemplateFunction);
     /*declarating the sliding bar*/
     var slidingAppBar;
     /******/
+
+    function getCustomberIdByName(name) {
+        var x = 0;
+        var compareName = data[x].Name;
+        while (name != compareName) {
+            x++;
+            compareName = data[x].Name;
+            if (x > data.count)
+                return -1;
+        }
+        return x;
+    }
+
+    function moreInfoButton_Click(e) {
+        var listView = document.getElementById("customerListView").winControl;
+        //listView.selection[lastInvokedTile].selected = false;
+        var temp = e.srcElement.parentElement.getElementsByClassName('itemid')[0];
+        var target = e.target;
+
+        // Load data to display
+        var x = getCustomberIdByName(temp.value);
+        var height = 20 + 2 + data[x].KPI.length * 26;
+        var counter = 0;
+        var flyOutContent = '<div style="width:420px; height:' + height + 'px; background-color:#000000; position:relative;">';
+        flyOutContent += '<div style="position:absolute; background-color:#FFFFFF; left:1px; top:1px; width:418px; height:20px; text-align:center;">' +
+            data[x].Name + '</div>';
+        var top = 22;
+        while ((counter < data[x].KPI.length)) {
+            flyOutContent += generateKPIDiv(data[x].KPI[counter][0], data[x].KPI[counter][1], data[x].KPI[counter][2], top, 240, 177);
+            counter++;
+            top += 26;
+        }
+
+        flyOutContent += '</div>';
+
+        var listFlyout = document.getElementById("confirmFlyout");
+
+        listFlyout.innerHTML = flyOutContent;
+        listFlyout.winControl.show(target);
+
+    }
+
+
+
 
     WinJS.UI.Pages.define("/pages/TileComparison/TileComparison.html", {
 
@@ -292,6 +344,7 @@
             slidingAppBar.getCommandById("showNextYear").addEventListener("click", function () { showNextYear(dimensionIndex); });
             slidingAppBar.getCommandById("showPreviousYear").addEventListener("click", function () { showPreviousYear(dimensionIndex); });
             slidingAppBar.getCommandById("showAllCards").addEventListener("click", function () { showAllCards(); });
+
             /*****************************************/
             customerCardsTileId = 0;
             document.body.querySelector("#compareButton").addEventListener("click", compareTile, false);
@@ -310,11 +363,7 @@
             customerCardsTileId = 0;
         }
 
-
     });
 
-
-
-
-
 })();
+
