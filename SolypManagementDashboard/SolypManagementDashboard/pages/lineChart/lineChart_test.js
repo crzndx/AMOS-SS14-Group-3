f7e6cc3b288453c412e55845e6f4299cf9160ea9
crzndx <crzndx@gmail.com>
2014-06-26 17:12:46 +0200

    linechart testing, prepare for release

--- SolypManagementDashboard/SolypManagementDashboard/pages/lineChart/lineChart_test.js
+++ SolypManagementDashboard/SolypManagementDashboard/pages/lineChart/lineChart_test.js
@@ -6,6 +6,55 @@
  * hint: use drag&drop from solution explorer to avoid wrongly spelled paths
  */
 
+
+var dataset1 = {
+    0: {
+        "myanmar": {
+            label: "Myanmar",
+            data: [[1988, 683994], [2006, 528692]]
+        },
+        "china": {
+            label: "China",
+            data: [[2004, 480451], [2005, 504638], [2006, 528692]]
+        }
+    },
+    1: {
+        "uk": {
+            label: "United Kingdom",
+            data: [[1988, 183194], [2005, 303468], [2006, 458495]]
+        }
+    }
+};
+
+var dataset2 = {
+    0: {
+        "Germany": {
+            label: "germany",
+            data: [[2011, 68394], [2031, 11191]]
+        },
+        "France": {
+            label: "France",
+            data: [[2034, 481451], [2005, 504631], [2026, 512312]]
+        }
+    },
+    1: {
+        "Poland": {
+            label: "Poland&/=)?)(?)(/&$§",
+            data: [[1988, 183194], [2105, 123465], [2104, 458335]]
+        }
+    },
+    2: {
+        "Bosnia": {
+            label: "Bosnia",
+            data: [[1288, 13394], [45, 1441], [1144, 35]]
+    }
+}
+};
+
+// for later use testing different datasets
+var datasets = [dataset1, dataset2];
+
+
 describe("General test", function () {
     it("just proves the proper work of Jasmine testing library", function () {
         expect(true).toBe(true);
@@ -13,19 +62,33 @@ describe("General test", function () {
 });
 
 
-describe("add function", function () {
-    it("test proper addition", function () {
-        var a = 1;
-        var b = 3;
-        var res = addthis(a, b);
-        expect(res).toBe(4);
+describe("update title", function () {
+    it("test updating title", function () {
+        // old title
+        var elem = document.getElementById("chartTitle").innerHTML;
+        updateChartTitle();
+        // updated title
+        var elemNew = document.getElementById("chartTitle").innerHTML;
+        expect(elem).not.toBe(elemNew);
     });
 });
 
-describe("string test", function () {
-    it("tests string", function () {
-        var name = "doesnotworkthatcrap";
-        expect(name).not.toBe("thatcrapdoesnotwork");
+describe("showNext/Prev test", function () {
+    it("tests showPrevious function", function () {
+        var i = -13;
+        while (i < 70) {
+            it("showNext test", function () {
+                // todo 
+                expect(true).toBe(true);
+            });
+
+            it("showPrevious test", function () {
+                // todo
+                expect(true).toBe(true);
+            });
+            i++;
+        }
+
     });
 });
 
@@ -62,14 +125,18 @@ describe("test data validation", function () {
         expect(data.length).not.toBe(0);
     });
 
-    it("access nonsense negative index", function () {
+    it("access nonsense index", function () {
         /* nonsense index */
         expect(data[-1]).toBe(undefined);
+        expect(data[3.141]).toBe(undefined);
+        expect(data[-312]).toBe(undefined);
+        expect(data["idonotexist"]).toBe(undefined);
     });
 
     it("access very big (unprobable) index", function () {
         /* nonsense index */
         expect(data[31231231232222123311122131231]).toBe(undefined);
+        expect(data[1337131121]).toBe(undefined);
     });
 
 
