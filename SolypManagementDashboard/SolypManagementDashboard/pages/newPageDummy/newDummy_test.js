44b778b5fc66e50713364f7ca60a131fb89c65da
crzndx <crzndx@gmail.com>
2014-06-26 17:21:22 +0200

    merged branches

---
+++ SolypManagementDashboard/SolypManagementDashboard/pages/newPageDummy/newDummy_test.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="../../lib//testing/Jasmine-2.0.0/Jasmine.js">
+/// <reference path="newDummyJS.js">
+
+// @TODO reference proper JS-file!
+
+describe("General test", function () {
+    it("just proves the proper work of Jasmine testing library", function () {
+        expect(true).toBe(true);
+    });
+});
